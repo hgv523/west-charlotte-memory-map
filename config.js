fc8866369fb4a6ebafa0936f8cbf43afc28270cd
gchen12 <gchen12@users.noreply.github.com>
2026-05-21 23:06:48 -0400

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 window.MemoryAtlasConfig = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ggjskyfxvdorsikloerr.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnanNreWZ4dmRvcnNpa2xvZXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDgzNTMsImV4cCI6MjA5NDk4NDM1M30.mkmwM7MiiZKFkFkDJwI3x3SizDgGGL7E-aTI2WGoeqU",
   supabaseTable: "memories",
   supabaseBucket: "memory-images",
   showSeedMemoriesWhenDatabaseEmpty: true,
   allowPublicDatabaseDeletes: false,
 };
-
